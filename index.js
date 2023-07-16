aac0421f20fbfe9a1ef1790471ec774b528873c5
Tanner Rhines <tannerrhines@protonmail.com>
2023-07-16 14:35:56 -0800

    wrote questions for command line

--- index.js
+++ index.js
@@ -1,2 +1,53 @@
 const filesystem = require('./node_modules/graceful-fs/graceful-fs')
 const inquirer = require("inquirer");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "choose up to 3 text characters:",
+    },
+    {
+        type: "input",
+        name: "text-color",
+        message: "Choose a color keyword or HEX number :",
+    },
+    {
+        type: "input",
+        name: "shape",
+        message: "Choose a color keyword or HEX number :",
+    },
+    {
+        type: "list",
+        name: "pixel-image",
+        message: "Which Pixel Image you would like for your logo?",
+        choices: ["Circle", "Square", "Triangle"],
+    },
+];
